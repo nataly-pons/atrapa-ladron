096b949aeb387fc4099a1d5cbfb714b90dea4d14
nataly-pons <nataliaponcequijano@gmail.com>
2026-03-31 00:04:40 -0600

    Mi primer commit: inicializando el proyecto

---
+++ src/test/java/com/ejemplo/ladron_librado/LadronLibradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.ladron_librado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadronLibradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
